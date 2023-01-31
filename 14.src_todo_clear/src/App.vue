522cdbed2adc48d8671195e4d7b58dbd4986c5eb
piratechu <piratechu@gmail.com>
2023-01-31 21:14:07 +0800

    clean todo project

---
+++ 14.src_todo_clear/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <!--把引入的components 放入此處-->
+    <div class="todo-container">
+        <div class="todo-wrap">
+            <FrameHeader></FrameHeader>
+            <ul class="todo-main">
+                <TodoItems></TodoItems>
+            </ul>
+            <FrameFooter></FrameFooter>
+        </div>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FrameHeader from "@/components/FrameHeader.vue";
+import FrameFooter from "@/components/FrameFooter.vue";
+import TodoItems from "@/components/TodoItems.vue";
+
+//利用 HomeView.vue 做各組件間的 props 交換資料
+export default {
+    name: "App",
+    components: {
+        FrameHeader,
+        FrameFooter,
+        TodoItems,
+    },
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
---
+++ 14.src_todo_clear/src/components/FrameFooter.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="delete-all">
+        <label>
+            <input class="input_checkbox" type="checkbox" />
+        </label>
+        <span> <span calss="span-font">已完成 ?? </span> / 全部 ?? </span>
+        <button class="btn-clear">清除已完成任务</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FrameFooter",
+    data() {
+        return {};
+    },
+};
+</script>
+<style scoped>
+.delete-all {
+    background-color: #eee;
+    border-radius: 6px;
+    padding: 20px;
+    display: flex;
+    align-items: center;
+}
+.btn-clear {
+    margin-top: 3px;
+    border: none;
+    background-color: #f44336;
+    color: white;
+    padding: 10px;
+    border-radius: 6px;
+    margin-left: 10px;
+    cursor: pointer;
+}
+
+.span-font {
+    font-size: 18px;
+}
+.input_checkbox {
+    width: 25px;
+    height: 25px;
+}
+</style>
