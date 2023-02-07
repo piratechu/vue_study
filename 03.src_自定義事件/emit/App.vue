19d7b89371c33878ae8577e413ebb8b49f0eb676
piratechu <piratechu@gmail.com>
2023-02-07 20:48:52 +0800

    vue emit

---
+++ 03.src_自定義事件/emit/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- 將子物件定義 emit 事件 customChange 定義在物件上，用一個 methods 接收 -->
+    <MyTextInput @customChange="emitFromChild"></MyTextInput>
+    <label>顯示的是父組件</label><br />
+    {{ msg }}
+</template>
+
+<script>
+import MyTextInput from "./components/MyTextInput.vue";
+import { ref } from "vue";
+
+export default {
+    name: "App",
+    components: { MyTextInput },
+    setup() {
+        let msg = ref("");
+        function emitFromChild(value) {
+            msg.value = value;
+        }
+        function grandChild(value) {
+            console.log(value);
+        }
+        return { msg, emitFromChild, grandChild };
+    },
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
