406e6d085752890b922c7a399a5707f9f93591db
piratechu <piratechu@gmail.com>
2023-02-20 21:05:09 +0800

    axios

---
+++ 08.src_axios使用範例/App - Axios.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <!-- {{ Users }} -->
+        <ul>
+            <li v-for="(user, index) in Users" :key="index">
+                {{ user.name.last }} - {{ user.email }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            Users: [],
+        };
+    },
+    methods: {
+        async getUser() {
+            const data = await axios.get(
+                "https://randomuser.me/api/?results=10"
+            );
+            return data;
+        },
+    },
+    mounted() {
+        this.getUser().then((resposne) => {
+            this.Users = resposne.data.results;
+            console.log(this.Users);
+        });
+    },
+};
+</script>
